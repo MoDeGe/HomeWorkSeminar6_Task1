511d6b53cf57a0b505be4449c7876fa50fe3d697
MoDeGe <md57@ya.ru>
2022-08-29 19:20:21 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите числа. Для окончания ввода введите esc");
+
+int j = 0;
+
+for(int i = 0; i < 500; i++)
+{
+string userNumber = Console.ReadLine();
+if(Int32.TryParse(userNumber, out int numValue))
+{
+  if(numValue > 0)
+  {
+    j++;   
+  }  
+}
+else
+{
+    break; 
+}
+}
+Console.WriteLine($"Вы ввели {j} числа(ел) больше нуля.");
